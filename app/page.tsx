2d6f22a4cf422e7d1769447e16790da9087bd209
liangliang2.jiang@tcl.com <liangliang2.jiang@tcl.com>
2025-01-24 11:09:50 +0800

    加点注释

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,14 @@ export default function Home() {
     }
   };
 
+  //这是一个事件
   useEffect(() => {
     fetchWallpapers();
   }, []);
 
+  //这是客户端渲染的方式，就是一上来不会拿数据。而是走到return拿页面，加载完之后会触发useEffect事件，那就会调fetchWallpapers方法获取数据。
+  //获取data之后，会通过useState方法，将数据设置到全局的变量 wallpapers中，然后通过wallpapers渲染到页面中。
+  //所以体感就是页面先出来，数据晚一点再出来。有异步的感觉 体验会好一点。但是seo就不友好，而且每次渲染都会请求一次接口。
   return (
     <div className="w-screen h-screen">
       <Header />
