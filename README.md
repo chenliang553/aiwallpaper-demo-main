8be118bc836cb52cf44493bae16e24e6d3a91c0a
liangliang2.jiang@tcl.com <liangliang2.jiang@tcl.com>
2025-01-24 10:33:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+## 本地开发
+
+1. 拉取代码
+
+```shell
+git clone https://github.com/trysai/aiwallpaper-demo
+```
+
+2. 进入项目根目录安装
+
+```shell
+cd aiwallpaper-demo
+pnpm install
+```
+
+3. 本地调试
+
+```shell
+pnpm dev
+```
+
+4. 本地访问
+
+http://localhost:3000
---
+++ app/api/gen-wallpaper/route.ts
@@ -0,0 +1,98 @@
+import { auth, currentUser } from "@clerk/nextjs";
+
+import { ImageGenerateParams } from "openai/resources/images.mjs";
+import { User } from "@/types/user";
+import { Wallpaper } from "@/types/wallpaper";
+import { downloadAndUploadImage } from "@/lib/s3";
+import { getOpenAIClient } from "@/service/openai";
+import { getUserCredits } from "@/service/order";
+import { insertUser } from "@/models/user";
+import { insertWallpaper } from "@/models/wallpaper";
+
+export async function POST(req: Request) {
+  const { description } = await req.json();
+
+  const user = await currentUser();
+  if (!user || !user.emailAddresses || user.emailAddresses.length === 0) {
+    return Response.json({
+      code: -2,
+      message: "user not login",
+    });
+  }
+
+  const user_email = user.emailAddresses[0].emailAddress;
+  const credits = await getUserCredits(user_email);
+  console.log("credits", credits);
+
+  if (credits.left_credits < 150) {
+    return Response.json({
+      code: -1,
+      message: "credits is not enough",
+    });
+  }
+
+  const nickname = user.firstName;
+  const avatarUrl = user.imageUrl;
+  const userInfo: User = {
+    email: user_email,
+    nickname: nickname || "",
+    avatar_url: avatarUrl,
+  };
+
+  // await insertUser(userInfo);
+
+  console.log("description", description);
+
+  const client = getOpenAIClient();
+
+  const img_size = "1792x1024";
+  const llm_name = "dall-e-3";
+  const llm_params: ImageGenerateParams = {
+    prompt: `generate a desktop wallpaper about: ${description}`,
+    model: llm_name,
+    n: 1,
+    quality: "hd",
+    response_format: "url",
+    size: img_size,
+    style: "natural",
+  };
+  const result = await client.images.generate(llm_params);
+
+  console.log("generate wallpaper result: ", result);
+
+  const raw_img_url = result.data[0].url; // openai dall-e img url
+  if (!raw_img_url) {
+    return Response.json({
+      code: -1,
+      message: "generate wallpaper failed",
+    });
+  }
+
+  const img_name = encodeURIComponent(description);
+  // const s3_img = await downloadAndUploadImage(
+  //   raw_img_url,
+  //   process.env.AWS_BUCKET || "aiwallpaper-demo",
+  //   `wallpapers/${img_name}.png`
+  // );
+  // const img_url = s3_img.Location;
+  const img_url = raw_img_url;
+
+  const created_at = new Date().toISOString();
+
+  const wallpaper: Wallpaper = {
+    user_email: user_email,
+    img_description: description,
+    img_size: img_size,
+    img_url: img_url,
+    llm_name: llm_name,
+    llm_params: JSON.stringify(llm_params),
+    created_at: created_at,
+  };
+  await insertWallpaper(wallpaper);
+
+  return Response.json({
+    code: 0,
+    message: "ok",
+    data: wallpaper,
+  });
+}
